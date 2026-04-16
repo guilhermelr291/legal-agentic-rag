8573d75186c8742c2381c5beaded22401a788005
guilhermelr291 <guilhermelr291dev@gmail.com>
2026-04-15 22:08:35 -0300

    feat: implement web search functionality using TavilySearch and optimize query generation

--- my_agent/utils/nodes.py
+++ my_agent/utils/nodes.py
@@ -3,6 +3,10 @@ from pydantic import BaseModel, Field
 from typing import Literal
 from langchain_openai import ChatOpenAI
 from my_agent.utils.state import MessagesState
+from my_agent.utils.state import GraphState
+from langchain_tavily import TavilySearch
+
+
 
 class RouteQuery(BaseModel):
     datasource: Literal["vectorstore", "web_search"] = Field(
@@ -19,11 +23,30 @@ prompt = ChatPromptTemplate.from_messages([
 router_chain = prompt | structured_llm
 
 
+
 def router_node(state: MessagesState) -> Literal["vectorstore", "web_search"]:
     question = state["messages"][-1].content
     result = router_chain.invoke({"question": question})
     return result.datasource
 
 
+
+
+tavily_tool = TavilySearch(max_results=5)
+
+
+llm_optimized_web_search = ChatOpenAI(model="gpt-5-nano")
+prompt_optimized_web_search = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant and your task is to transform the user question into a search query optimized for tavily search."),
+    ("human", "User question: {question}"),
+])
+
+optimized_web_search_chain = prompt_optimized_web_search | llm_optimized_web_search
+
+def web_search_node(state: GraphState) -> GraphState:
+    question = state["messages"][-1].content
+    optimized_query = optimized_web_search_chain.invoke({"question": question})
+    tavily_results = tavily_tool.run(optimized_query.content)
+    return {"documents": tavily_results}
 
 
--- my_agent/utils/state.py
+++ my_agent/utils/state.py
@@ -1,7 +1,8 @@
 from langchain_core.messages import AnyMessage
 from typing import TypedDict, Annotated
 import operator
-
+from typing import List
 
 class GraphState(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
+    documents: List[str]
